1554c258122c5db77d2fbdf464807ee96cb44a78
vanxey <82112436+vanxey@users.noreply.github.com>
2026-02-03 23:08:27 +0100

    setup: add storyblok dependencies and set up first block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,30 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import { storyblok } from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import mkcert from 'vite-plugin-mkcert'
 
-// https://astro.build/config
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+const { STORYBLOK_DELIVERY_API_TOKEN } = loadEnv(
+  import.meta.env.MODE,
+  process.cwd(),
+  '',
+);
+
 export default defineConfig({
-  integrations: [],
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_DELIVERY_API_TOKEN,
+      apiOptions: {
+        region: 'eu',
+      },
+      components: {
+        page: "/storyblok/Page",
+        hero: "/components/Hero", 
+      },
+    }),
+  ],
+  output: 'server',
+  vite: {
+    plugins: [ mkcert() ]
+  },
 });
